567a1b79782ff744e0bceb7e79f136dd4c161dc7
Isaias Pomales <impomales@gmail.com>
2021-06-25 18:43:44 -0400

    set up IBOutlets and IBActions for CalculatorViewController

---
+++ Tipsy/CalculatorViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func tipChanged(_ sender: Any) {
+    }
+    
+    @IBAction func stepperVAlueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: Any) {
+    }
+}
+
