f206a2ce12c87441f4ee02d461b6fe2384a79b54
Isaias Pomales <impomales@gmail.com>
2021-07-01 16:10:00 -0400

    user can calculate tip

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -14,20 +14,44 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
+    @IBOutlet weak var splitStepper: UIStepper!
+    
+    var tipAmount: Double = 0.1
+    var splitValue: Double = 2.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        splitStepper.value = splitValue
     }
 
 
-    @IBAction func tipChanged(_ sender: Any) {
+    @IBAction func tipChanged(_ sender: UIButton) {
+        billTextField.endEditing(true)
+        
+        deselectAllTipButtons()
+        sender.isSelected = true
+        
+        let tipStrValue = String(sender.currentTitle!.dropLast())
+        tipAmount = Double(tipStrValue)! / 100
     }
     
-    @IBAction func stepperVAlueChanged(_ sender: Any) {
+    @IBAction func stepperVAlueChanged(_ sender: UIStepper) {
+        splitValue = sender.value
+        splitNumberLabel.text = String(format: "%.0f", splitValue)
     }
     
-    @IBAction func calculatePressed(_ sender: Any) {
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let billTotal = Double(billTextField.text!) ?? 0
+        let billTotalWithTip = billTotal + billTotal * tipAmount
+        let amountOwed = (billTotalWithTip / splitValue).rounded()
+        print(amountOwed)
+    }
+    
+    private func deselectAllTipButtons() {
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
     }
 }
 
